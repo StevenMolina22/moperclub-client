3411d859074ccb181814d142dd59460e4653f97a
Steven Molina <steven.molina.slayer@gmail.com>
2024-03-06 10:17:30 -0300

    main tsx update on routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import Router from "./pages/Router.tsx";
+// import "./scss/index.scss";
+
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     <Router />
+//   </React.StrictMode>
+// );
+
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import Router from "./pages/Router.tsx";
 import "./scss/index.scss";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.render(
   <React.StrictMode>
     <Router />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
+
